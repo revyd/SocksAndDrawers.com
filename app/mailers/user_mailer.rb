ebfd49feacd8656af4d911192878a5b398a3c10e
Dave Olson <dave@daveolson.ca>
2013-04-19 22:52:29 -0700

    rails_apps_composer: initial commit

--- app/mailers/user_mailer.rb
+++
@@ -1,7 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "notifications@example.com"
-  
-  def expire_email(user)
-    mail(:to => user.email, :subject => "Subscription Cancelled")
-  end
-end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SocksAndDrawersCom::Application.config.secret_token = '89b396d3068add17e82dfb7530795b1ec6570ee9746cbad15fbfc2f3634b38788c08e88808aef2ac678cd9548e5cc6d9f3089a1e5980c505bbf3ab60912005b3'
+SocksAndDrawersCom::Application.config.secret_token = '3d634f2c5dc63d9c5b6f7fbd3bd35823c66e595ae81ef57aa9b020af47c3808d0973fd81171040bd7144fa3bb50881ff52035cfd885d34732971ec8ae79ca522'
