5da3bfd9db6be17a313568d8ab9e75338a078b04
Dave Olson <dave@daveolson.ca>
2013-04-23 16:09:50 -0700

    Fixed config error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ SocksAndDrawersCom::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'http://aqueous-fortress-5057.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'aqueous-fortress-5057.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
